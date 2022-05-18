a08e96553440a2ca494773823416cbbb9a36ed78
Adam Hoffman <adam@gravitybit.com>
2022-05-18 16:09:09 -0400

    Initial commit, solved.

---
+++ main.py
@@ -0,0 +1,12 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    # The provided code stub reads two integers,  and , from STDIN.
+    a = int(raw_input())
+    b = int(raw_input())
+
+# The first line should contain the result of integer division, a // b.
+print(a // b)
+
+# The second line should contain the result of float division,  a / b.
+print(a / b)
